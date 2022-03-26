94d59c81078add252a11d9b95d228e053338ea0a
Ditte Lykke Hansen <dittehansen143@gmail.com>
2022-03-26 21:09:15 +0100

    frontpage styling

--- src/components/Frontpage/Frontpage.js
+++ src/components/Frontpage/Frontpage.js
@@ -1,7 +1,39 @@
 import React from "react";
+import './Frontpage.css';
+import addNewList from '../../assets/img/add-new-list.svg';
+import list from '../../assets/img/small-list.svg';
+import sharedList from '../../assets/img/shared-list.svg';
+
 
 const Frontpage = () => {
-  return <div>Frontpage</div>;
+  return (
+  <div className="frontpage">
+    <div className="profileFront">
+      <p className="profileIcon">DH</p>
+      <p className="profileName">Ditte Hansen</p>
+    </div>
+    <hr className="hr" />
+    <img className="addNewListIcon" src={addNewList} alt="Tilføj ny liste" />
+    <section>
+      <h3 className="listCategory">Lister oprettet af mig</h3>
+      <hr className="hrList" />
+      <div className="list">
+      <img className="listIcon" src={list} alt="Liste ikon" />
+      <p>Pakkeliste</p>
+      </div>
+    </section>
+    <section>
+      <h3 className="listCategory">Lister delt med mig</h3>
+      <img className="sharedListIcon" src={sharedList} alt="Delt med mig - ikon" />
+      <hr className="hrList" />
+      <div className="list">
+        <img className="listIcon" src={list} alt="Liste ikon" />
+        <p>Indkøbsliste</p>
+      </div>
+    </section>
+
+  </div>
+  );
 };
 
 export default Frontpage;
