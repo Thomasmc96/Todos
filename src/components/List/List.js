ea0ccee26e087b2a581b855cef513ddb591bf555
Ditte Lykke Hansen <dittehansen143@gmail.com>
2022-04-03 21:36:08 +0200

    added more to the side

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,20 +1,23 @@
 import React, {useState, useEffect} from "react";
 import "./List.css";
-import back from "../../assets/img/back.svg";
-import share from "../../assets/img/share.svg";
+import backIcon from "../../assets/img/back.svg";
+import shareIcon from "../../assets/img/share.svg";
+import notDoneIcon from "../../assets/img/not-done.svg";
+import editIcon from "../../assets/img/edit.svg";
+import addIcon from "../../assets/img/add.svg";
 import axios from "axios";
 import { useParams, Link } from "react-router-dom";
 
 const List = () =>{
     const {id}=useParams()
 
-    const [list, setList] = useState({lists_id: id, name: ''})
+    const [list, setList] = useState({list_name: '', products: [{completed: '', name: '', products_id: ''}]})
     
     useEffect(() => {
-        axios(`http://localhost:8000/server/lists/read.php?lists_id=${id}`)
+        axios(`http://localhost:8000/server/products/read.php?lists_id=${id}`)
           .then((result) => {
-            console.log(result.data[0]);
-            setList(result.data[0])
+            console.log(result.data);
+            setList(result.data)
           })
           .catch((error) => {
             console.log(error);
@@ -25,17 +28,33 @@ const List = () =>{
         <div className="listSection">
             <div className="firstSection">
                 <Link to={'/'} className="backSection">
-                    <img id="back" src={back} alt="Tilbage ikon" />
+                    <img id="back" src={backIcon} alt="Tilbage ikon" />
                     <p>Tilbage</p>
                 </Link>
                 <div className="shareSection">
                     <p>Del liste</p>
-                    <img id="share" src={share} alt="Del liste ikon" />
+                    <img id="share" src={shareIcon} alt="Del liste ikon" />
                 </div>
             </div>
             <hr className="hr" />
-            <h1>{list.name}</h1>
+            <h1>{list.list_name}</h1>
             <hr className="hr" />
+            {list.products.map((product)=>{
+                return(
+                    <div className="todoProduct" key={product.products_id}>
+                        <div className="doneOrNot">
+                            <img id="notDoneIcon" src={notDoneIcon} alt="Ikke-færdig ikon" />
+                            <p>{product.name}</p>
+                        </div>
+                        <img id="editIcon" src={editIcon} alt="Redigér ikon" />
+                    </div>
+                )
+
+            })}
+            <div className="addSection">
+                <img src={addIcon} alt="Tilføj opgave ikon" />
+                <p>Tilføj et punkt</p>
+            </div>
         </div>
     )
 }
