5691e20c9c8fc7c6ddb9089ad6c63a4e69431e0a
Thomas Christensen <tc@tales.dk>
2022-09-30 09:05:08 +0200

    Notifications wip

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,13 +4,18 @@ import todo from "../../assets/img/todos.svg";
 import settings from "../../assets/img/settings.svg";
 import { Link } from "react-router-dom";
 import Settings from "./Settings.js";
+import Notifications from './Notifications.js';
 import InstallPWAButton from "../Utilities/InstallPWAButton";
 import DeleteProfile from "./DeleteProfile";
 
 const Header = () => {
   const [toggleSettings, setToggleSettings] = useState(false);
+  const [toggleNotifications, setToggleNotifications] = useState(false);
   const [showDeleteProfile, setShowDeleteProfile] = useState(false);
 
+  const showNotifications = () => {
+    setToggleNotifications(!toggleNotifications);
+  };
   const showSettings = () => {
     setToggleSettings(!toggleSettings);
   };
@@ -31,12 +36,29 @@ const Header = () => {
         </Link>
       )}
       {localStorage.getItem("users_id") !== null ? (
-        <img
-          onClick={showSettings}
-          id="settingsIcon"
-          src={settings}
-          alt="Instillinger ikon"
-        />
+        <div className="headerIcons">
+          <div className="notificationContainer">
+
+            <img
+              onClick={showNotifications}
+              class="icon"
+              src={settings}
+              alt="Notifikationer ikon"
+            />
+            {toggleNotifications && (
+              <Notifications
+                showNotifications={showNotifications}
+              />
+            )}
+          </div>
+          <img
+            onClick={showSettings}
+            class="icon"
+            src={settings}
+            alt="Instillinger ikon"
+          />
+        </div>
+
       ) : (
         <InstallPWAButton />
       )}
