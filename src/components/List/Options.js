012af600efd7b83155d73d474a6b24afd10c8215
Thom9521 <Thomas96mc@gmail.com>
2022-10-30 09:08:46 +0100

    Leave list visual bug fix

--- src/components/List/Options.js
+++ src/components/List/Options.js
@@ -1,39 +1,57 @@
-import React from 'react'
+import React from "react";
 import shareIcon from "../../assets/img/share.svg";
 import leaveIcon from "../../assets/img/leave.svg";
 import deleteIcon from "../../assets/img/delete.svg";
 import membersIcon from "../../assets/img/shared-list.svg";
 
 const Options = (props) => {
-    return (
-        <div className='options popup'>
-            <span className="cross" onClick={props.showOptions}>x</span>
-            <h2>Valgmuligheder</h2>
-            <div>
-                <button type='button' onClick={props.showMembers}>
-                    <img className='icon' src={membersIcon} alt="Medlemmer ikon" />
-                    <p>Medlemmer</p>
-                </button>
-                {props.list.users_id === localStorage.getItem("users_id") && (
-                    <React.Fragment>
-                        <button type='button' onClick={props.showShareList}>
-                            <img className="shareIcon icon" src={shareIcon} alt="Del liste ikon" />
-                            <p>Del liste</p>
-                        </button>
-                        <button type='button' className='danger' onClick={props.showDeleteList}>
-                            <img className="deleteList icon" src={deleteIcon} alt="Slet liste ikon" />
-                            <p>Slet liste</p>
-                        </button>
-                    </React.Fragment>
-                )}
-                {props.list.users_id !== localStorage.getItem("users_id") && (
-                    <button className='danger' onClick={props.showLeaveList}>
-                        <p>Forlad liste</p>
-                        <img className='leaveIcon icon' src={leaveIcon} alt="Forlad liste ikon" />
-                    </button>
-                )}
-            </div>
-        </div>
-    )
-}
-export default Options
+  return (
+    <div className="options popup">
+      <span className="cross" onClick={props.showOptions}>
+        x
+      </span>
+      <h2>Valgmuligheder</h2>
+      <div>
+        <button type="button" onClick={props.showMembers}>
+          <img className="icon" src={membersIcon} alt="Medlemmer ikon" />
+          <p>Medlemmer</p>
+        </button>
+        {props.list.users_id === localStorage.getItem("users_id") && (
+          <React.Fragment>
+            <button type="button" onClick={props.showShareList}>
+              <img
+                className="shareIcon icon"
+                src={shareIcon}
+                alt="Del liste ikon"
+              />
+              <p>Del liste</p>
+            </button>
+            <button
+              type="button"
+              className="danger"
+              onClick={props.showDeleteList}
+            >
+              <img
+                className="deleteList icon"
+                src={deleteIcon}
+                alt="Slet liste ikon"
+              />
+              <p>Slet liste</p>
+            </button>
+          </React.Fragment>
+        )}
+        {props.list.users_id !== localStorage.getItem("users_id") && (
+          <button className="danger" onClick={props.showLeaveList}>
+            <img
+              className="leaveIcon icon"
+              src={leaveIcon}
+              alt="Forlad liste ikon"
+            />
+            <p>Forlad liste</p>
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Options;
