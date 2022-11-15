af21755066663782d5fa1f9e89065c52033dedbc
Thom9521 <thomas96mc@gmail.com>
2022-11-15 16:28:20 +0100

    Dark mode WIP

--- src/components/Header/Settings.js
+++ src/components/Header/Settings.js
@@ -1,8 +1,17 @@
-import React, { useEffect, useCallback } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import environment from "../../environment";
 import InstallPWAButton from "../Utilities/InstallPWAButton";
 
 const Settings = (props) => {
+
+  const [useDarkMode, setUseDarkMode] = useState(false);
+
+  const handleDarkMode = () => {
+    setUseDarkMode(!useDarkMode);
+
+    localStorage.setItem("theme", !useDarkMode ? "dark" : "light");
+  }
+
   const logout = () => {
     localStorage.removeItem("users_id");
     localStorage.removeItem("name");
@@ -27,12 +36,18 @@ const Settings = (props) => {
   );
 
   useEffect(() => {
+
+    let theme = localStorage.getItem("theme");
+    if(theme !== null){
+      document.body.className = theme;
+    }
+    
     window.addEventListener("click", hidePopup);
 
     return () => {
       window.removeEventListener("click", hidePopup);
     };
-  }, [hidePopup]);
+  }, [hidePopup, useDarkMode]);
 
   const version = environment[1];
 
@@ -42,6 +57,12 @@ const Settings = (props) => {
         x
       </span>
       <h2>Indstillinger</h2>
+      <div className="darkModeToggleContainer">
+        <label className="switch">
+          <input type="checkbox" name="switch" checked={useDarkMode} onChange={handleDarkMode}/>
+          <span className="slider round"></span>
+        </label>
+      </div>
       <div>
         <InstallPWAButton />
         <button
