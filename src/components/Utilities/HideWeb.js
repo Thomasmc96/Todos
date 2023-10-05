66f2c6bdb05fdc2bc0b51120781223625ebd6bdc
Thom9521 <Thomas96mc@gmail.com>
2023-10-05 22:08:28 +0200

    hideweb changes

--- src/components/Utilities/HideWeb.js
+++ src/components/Utilities/HideWeb.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import logo from "../../assets/img/todos.svg";
+import React from "react";
+import logo from "../../assets/img/new-font-logo.svg";
 
 const HideWeb = () => {
-    return (
-        <div className='hideWeb'>
-            <img src={logo} alt="Todos logo" />
-            <h2>Skift til mobilen (lodret) for nu {":-)"}</h2>
-        </div>
-    )
-}
-export default HideWeb
+  return (
+    <div className="hideWeb">
+      <img src={logo} alt="Todos logo" />
+      <h2>Skift til mobilen (lodret) for nu {":-)"}</h2>
+    </div>
+  );
+};
+export default HideWeb;
